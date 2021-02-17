7e08dcc7ae81a9410312e60449978ceec8852961
Grey <lamtrinhthong@gmail.com>
2021-02-17 23:42:46 +0700

    Upload layout for login page

---
+++ java/com/myclass/util/JspPath.java
@@ -0,0 +1,19 @@
+package com.myclass.util;
+
+public class JspPath {
+	/* url jsp file for loginController */
+	public static final String JSP_LOGIN 		= "/WEB-INF/views/login/index.jsp";
+	
+	/* url jsp file for roleServlet */
+	public static final String JSP_HOME 		= "/WEB-INF/views/home/index.jsp";
+	
+	/* url jsp file for roleServlet */
+	public static final String JSP_ROLE 		= "/WEB-INF/views/role/index.jsp";
+	public static final String JSP_ROLE_ADD 	= "/WEB-INF/views/role/add.jsp";
+	public static final String JSP_ROLE_EDIT 	= "/WEB-INF/views/role/edit.jsp";
+	
+	/* URL jsp file for userServlet */
+	public static final String JSP_USER 		= "/WEB-INF/views/user/index.jsp";
+	public static final String JSP_USER_ADD 	= "/WEB-INF/views/user/add.jsp";
+	public static final String JSP_USER_EDIT 	= "/WEB-INF/views/user/edit.jsp";
+}
